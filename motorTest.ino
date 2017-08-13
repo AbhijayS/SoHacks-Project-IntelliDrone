770677ef75e729640f24fcd9d6e9c88d51bad347
Abhijay Saini <abhijay.saini@gmail.com>
2017-08-13 17:19:00 -0500

    Arduino Program File motorTest
    Takes Serial Input from computer and sends data to RC Transmitter

---
+++ motorTest.ino
@@ -0,0 +1,35 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.flush();
+  // put your main code here, to run repeatedly:
+  char key;
+
+  if (Serial.available() > 0){
+    key = Serial.read();
+    //Serial.print("You typed: ");
+    //Serial.println(key);
+    
+    if(key == 119){
+      //motor moves forward
+      Serial.println("Up");
+    }
+    if(key == 97){
+      //motor moves left
+      Serial.println("Left");
+    }
+    if(key == 115){
+      //motor moves down
+      Serial.println("Down");
+    }
+    if(key == 100){
+      //motor moves right
+      Serial.println("Right");
+    }
+    
+  }
+
+}
